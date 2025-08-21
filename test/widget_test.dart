0af1568927e9c53603242cacb30f95acae4352fa
tianchoy <tianchoy@live.com>
2025-08-21 22:05:08 +0800

    test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:zd_iot/app/app.dart';
 
-import 'package:zd_iot/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
